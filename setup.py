c87d6149c9c58da4b2b22610414ff687339e8c9d
Steven Murray <steven.jeanette.m@gmail.com>
2013-05-20 14:36:22 +0800

    added setup and readme

---
+++ setup.py
@@ -0,0 +1,21 @@
+'''
+Created on May 20, 2013
+
+@author: Steven
+'''
+from setuptools import setup, find_packages
+version = '1.0.0'
+
+setup(
+    name="HMFcalc",
+    version=version,
+    packages=find_packages(),
+    install_requires=[],
+    requires=['numpy', 'pandas'],
+    author="Steven Murray",
+    author_email="steven.jeanette.m@gmail.com",
+    description="A halo mass function calculator web-app in django",
+    keywords="halo mass function",
+    url="https://github.com/steven-murray/HMFcalc",
+    # could also include long_description, download_url, classifiers, etc.
+)
