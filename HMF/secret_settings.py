b42cd1ebb12edf54f676fee751fbd695bc62d74e
Steven Murray <steven.g.murray@asu.edu>
2020-06-25 22:10:32 -0700

    fix: update requirements

---
+++ HMF/secret_settings.py
@@ -0,0 +1,11 @@
+# Analytics settings
+GOOGLE_ANALYTICS_PROPERTY_ID = "UA-139011886-1"
+
+# Email settings
+MY_EMAIL = "steven.g.murray@asu.edu"  # Address that emails *go to*
+HOST_EMAIL = "hmfcalc.new@gmail.com"  # Email address that emails *come from*
+EMAIL_HOST_PASSWORD = "1l0veG0d."  # password of *from* email.
+EMAIL_PORT = 587  # Port to use for the SMTP server defined in EMAIL_HOST.
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = "0jyd4=ov)av@e%e(kz9^8b9l+xlljmd%q6q6z*uc64uz&8r+b="
